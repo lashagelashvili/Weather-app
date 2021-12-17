c87d7dfbc31755b800bbaad59d0e2564e1c91387
lashagelashvili <las.gelashvili@gmail.com>
2021-12-17 15:58:27 +0400

    interface update

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,4 +1,29 @@
-export interface Api {
-    location: object,
-    current: object,
+export interface Weather {
+    location: Location,
+    current: Current,
+}
+
+export interface Location {
+    name: string;
+    region: string;
+    country: string;
+    lat: number;
+    lon: number;
+    localtime: number;
+}
+
+export interface Current {
+    temp_c: number;
+    temp_f: number;
+    condition: Condition;
+    wind_mph: number;
+    wind_kph: number;
+    humidity: number;
+    cloud: number;
+}
+
+export interface Condition {
+    text: string;
+    icon: string;
+    code: number;
 }
