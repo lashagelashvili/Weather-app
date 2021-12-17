14a769c7a07e029efa141c1978d62eeffb0832ec
lashagelashvili <las.gelashvili@gmail.com>
2021-12-17 15:58:14 +0400

    update ui

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Api } from './weather';
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
 
 @Component({
   selector: 'app-weather',
@@ -10,38 +11,20 @@ import { Api } from './weather';
 
 export class WeatherComponent implements OnInit {
   private url = 'http://api.weatherapi.com/v1/current.json?key=bb559d3852304c81af7180800212011&q=';
-  temperature: number = 0;
-  city: string = '';
-  date: string = '';
-  cloud: number = 0;
-  humidity: number = 0;
-  wind: number = 0;
-  weather: string = '';
-  apiResult: any;
-  iconUrl: string = '';
-
+  weatherData: Weather;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  getData(city: string): Observable<Weather> {
+    return this.http.get<Weather>(this.url + city);
+  }
+
   onClick(city: string) {
-    this.http.get<Api>(this.url + city).subscribe(data => {
-      this.apiResult = data;
-      console.log(this.apiResult);
-
-      this.temperature = this.apiResult.current.temp_c;
-      this.city = this.apiResult.location.name;
-      this.date = this.apiResult.location.localtime;
-
-      this.humidity = this.apiResult.current.humidity;
-      this.cloud = this.apiResult.current.cloud;
-      this.wind = this.apiResult.current.wind_kph;
-
-      this.weather = this.apiResult.current.condition.text;
-      this.iconUrl = this.apiResult.current.condition.icon;
-    });
-
+    this.getData(city).subscribe((data: Weather) => {
+      this.weatherData = data;
+    })
   }
 }
